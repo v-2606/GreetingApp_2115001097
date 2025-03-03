c543316b816a5830057ecfe0622273360867f5a9
varsha <varshasinghh99@gmail.com>
2025-03-03 19:14:38 +0530

    [Varsha] adding UC1 codes on UC1 branch

---
+++ ModelLayer/Model/RequestModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLayer.Model
+{
+  public class RequestModel
+    {
+
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
---
+++ RepositoryLayer/Service/GreetingRL.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RepositoryLayer.Interface;
+
+namespace RepositoryLayer.Service
+{
+    public class GreetingRL:IGreetingRL
+    {
+    }
+}
