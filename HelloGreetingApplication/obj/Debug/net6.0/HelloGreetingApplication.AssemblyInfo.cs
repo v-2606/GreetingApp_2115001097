e4aaac32d0360740002b605277bfcaae9a30db43
varsha <varshasinghh99@gmail.com>
2025-03-06 14:52:59 +0530

    [Varsha] Adding work  of UC9 from main

--- HelloGreetingApplication/obj/Debug/net6.0/HelloGreetingApplication.AssemblyInfo.cs
+++ HelloGreetingApplication/obj/Debug/net6.0/HelloGreetingApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloGreetingApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2177f7e2bd8083894f855e27a2884931ba493648")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61a410b0c2b1285682de0aa47c580ad3617e80b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloGreetingApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloGreetingApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
