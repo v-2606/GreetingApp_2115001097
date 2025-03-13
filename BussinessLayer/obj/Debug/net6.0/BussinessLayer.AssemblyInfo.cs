a47b736f06d5c49039d2c552366f712bab4d84f1
varsha <varshasinghh99@gmail.com>
2025-03-13 11:43:41 +0530

    merging work of UC11(jwt and forget / reset) to main

--- BussinessLayer/obj/Debug/net6.0/BussinessLayer.AssemblyInfo.cs
+++ BussinessLayer/obj/Debug/net6.0/BussinessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BussinessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e9a2111819db6c588205c96c276afafdbef6b2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84d63c6e0e7591998bce9513a8c4cd2af96ae0ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("BussinessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BussinessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
